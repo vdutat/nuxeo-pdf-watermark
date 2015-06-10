995c978c59618bae5bc05395181fcedafdb495dd
Vincent Dutat <vdutat@nuxeo.com>
2015-06-10 15:21:29 -0700

    add basic webengine app. files

---
+++ src/main/java/org/nuxeo/ecm/platform/addon/jaxrs/PdfWatermarkRoot.java
@@ -0,0 +1,42 @@
+/*
+ * (C) Copyright ${year} Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     vdutat
+ */
+
+package org.nuxeo.ecm.platform.addon.jaxrs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.nuxeo.ecm.webengine.model.WebObject;
+import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
+
+
+/**
+ * The root entry for the WebEngine module. 
+ * 
+ */
+@Path("/pdfwatermark")
+@Produces("text/html;charset=UTF-8")
+@WebObject(type="PdfWatermarkRoot")
+public class PdfWatermarkRoot extends ModuleRoot {
+
+    @GET
+    public Object doGet() {
+        return getView("index");
+    }
+
+}
